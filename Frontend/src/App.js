31537dbf63d794f36721b6d0d98ece659e71e8aa
Yash <yashdatir@Yashs-MacBook-Pro.local>
2020-10-11 23:26:37 +0530

    Initial commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class App extends React.Component {
+  render(){
+  return (
+    <React.Fragment>
+      <input type="text" placeholder="Text Message" /><button>Send</button>
+    </React.Fragment>
+  );
+  }
+}
+export default App;
