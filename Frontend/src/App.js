03eaf22bbd732b71977e15cbe3665f7255b02b01
Yash <yashdatir@Yashs-MacBook-Pro.local>
2020-10-12 00:02:37 +0530

    Hello World by socket

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:3001')
 class App extends React.Component {
+  componentDidMount(){
+    socket.on('chat-message', data => {
+    console.log(data)
+  })
+  }
   render(){
   return (
     <React.Fragment>
